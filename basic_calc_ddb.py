f448ee9547e06a2dd83e4430f0bacacd584287e9
Bryan Der <bryan@notch.io>
2016-09-30 14:06:07 -0400

    yapf formatting

--- basic_calc_ddb.py
+++ basic_calc_ddb.py
@@ -2,40 +2,33 @@ import time
 from datetime import datetime
 import boto3
 DDB = boto3.resource('dynamodb').Table('calculations')
-	
+
+
 def handler(event, context):
     a = int(event['a'])
     b = int(event['b'])
     op = event['op']
     c = None
-    
-    if op == '+' or op ==  'add':
+
+    if op == '+' or op == 'add':
         c = a + b
-    elif op == '-' or op ==  'subtract':
+    elif op == '-' or op == 'subtract':
         c = a - b
-    elif op == '*' or op ==  'multiply':
+    elif op == '*' or op == 'multiply':
         c = a * b
-    elif op == '/' or op ==  'divide':
+    elif op == '/' or op == 'divide':
         c = a / b
     else:
         return "operation not allowed"
-        
-    res = {
-        'a': a,
-        'b': b,
-        'op': op,
-        'c': c
-    }
-    
-    
+
+    res = {'a': a, 'b': b, 'op': op, 'c': c}
+
     now = datetime.utcnow()
     timestamp = int(time.mktime(now.timetuple()))
     res['timestamp'] = str(timestamp)
-    
-    item = {
-        'Item': res
-    }
-    
+
+    item = {'Item': res}
+
     DDB.put_item(**item)
 
     return res
